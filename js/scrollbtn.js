b73c19ed9e1ec259a833abbf354c835326527bf1
aabbcc01 <pontoppidan1917@yahoo.co.jp>
2021-02-13 10:19:48 +0900

    10.2

--- js/scrollbtn.js
+++ js/scrollbtn.js
@@ -3,7 +3,7 @@
        // スクロールしたときに実行
        $(window).scroll(function () {
           // 目的のスクロール量を設定(px)
-          var TargetPos = 1200;
+          var TargetPos = 1000;
           // 現在のスクロール位置を取得
           var ScrollPos = $(window).scrollTop();
           // 現在位置が目的のスクロール量に達しているかどうかを判断
