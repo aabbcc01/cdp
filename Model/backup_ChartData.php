3c8fe5ff6d7aa5e11ff244805486bd05fc472031
aabbcc01 <pontoppidan1917@yahoo.co.jp>
2021-03-18 18:33:23 +0900

    11.3 ChartData modified

--- Model/backup_ChartData.php
+++ Model/backup_ChartData.php
@@ -7,8 +7,8 @@ function getChartData($params,$table){
 	//DBコネクタを生成
     $db=getDb();
     
-    
     $where=[];
+    $year_comp_vctype=[];
 
     if(isset($params['year']) && is_array($params['year'])){
         $year=[];
@@ -18,7 +18,7 @@ function getChartData($params,$table){
         //sql用の変数。
         $year=implode(',',$year);
         $yearSql='year IN ('.$year.') ';
-        $where[]=$yearSql;
+        $year_comp_vctype[]=$yearSql;
     }
 
     if(isset($params['comp_id']) && is_array($params['comp_id'])){
@@ -31,7 +31,7 @@ function getChartData($params,$table){
          //sql用の変数。company_id=xx OR company_id=xx ...
         $compid=implode(',',$comp_id);
         $compidSql='company_id IN ('.$compid.') ';
-        $where[]=$compidSql;
+        $year_comp_vctype[]=$compidSql;
     } 
     
     if(!empty($params['value_chain'])){
@@ -44,14 +44,21 @@ function getChartData($params,$table){
          //sql用の変数。
          $vctype=implode(',',$vc_type);
          $vctypeSql='vc_type IN ('.$vctype.') ';
-         $where[]=$vctypeSql;
+         $year_comp_vctype[]=$vctypeSql;
      } 
 
-    if(!empty($where)){
-        /* $whereSql の最終形態：はyear IN (x,y) AND company_id IN (x,y) 
-         AND vc_type IN (x,y)    */  
-        $whereSql=implode(' AND ',$where);
-        $sql =$db->prepare('select * from '.$table.' where ' .$whereSql.' order by year desc,company,identifier asc');
+    // (A AND B) のsqlを作成： (year IN (2020,2019,20xx,...) AND company_id IN (14,15,...) AND vc_type ID (1,2,...))
+    if(!empty($year_comp_vctype)){
+        $year_comp_vctypeSql=implode(' AND ',$year_comp_vctype);
+        $where[]='('.$year_comp_vctypeSql.')';
+    }
+
+    if(!empty($year_comp_vctype)){
+        /* $whereSql の最終形態：は(year IN (x,y) AND company_id IN (x,y) 
+         AND vc_type IN (x,y) )    */  
+        $whereSql = implode(' AND ', $where);
+       // print_r('$whereSql= '.$whereSql."<br>");
+        $sql =$db->prepare('select * from '.$table.' where '.'(' .$whereSql.') order by year desc,company,identifier asc');
 
     }else{
         //全てのチェックボックスにチェックがされていない場合：全件検索
