1358d57bdb81c4853aa2ea528380120d139ecf67
aabbcc01 <pontoppidan1917@yahoo.co.jp>
2021-04-05 02:31:33 +0900

    test

--- DbManager.php
+++ DbManager.php
@@ -1,7 +1,7 @@
 <?php
 function getDb() {
-  $dsn = 'mysql:dbname=cdp; host=%; charset=utf8';
-  $usr = 'cdp';
+  $dsn = 'mysql:dbname=cdp; host=localhost; charset=utf8';
+  $usr = 'root';
   $passwd = 'Tokissme?';
 
     $db = new PDO($dsn, $usr, $passwd);
