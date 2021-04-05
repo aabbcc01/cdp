ef36594be4f04e48e0c6fb884d1ad86a202c87bf
aabbcc01 <pontoppidan1917@yahoo.co.jp>
2021-04-05 12:57:50 +0900

    a

--- register/account_in.php
+++ register/account_in.php
@@ -1,11 +1,5 @@
 
-<?php
-    session_start();
-    require_once '../DbManager.php';
-    require_once '../Encode.php';
-    require_once '../Function/verifyUser.php';
-    verifyUser($_SESSION['user'],getDb());
-?>
+
 
 <!DOCTYPE html>
 <html>
