af9c17126f643fd670edf9d9eabeaef0763dd0fc
aabbcc01 <pontoppidan1917@yahoo.co.jp>
2021-04-05 16:38:42 +0900

    a

--- result.php
+++ result.php
@@ -85,22 +85,7 @@ $CdpData = getCDP($_GET);
 			}else { return "";}
 		}
 		
-		function make_td(int $header,int $border,int $colspan,string $setUnderb,string $answer){
-			
-			$h_answer=htmlspecialchars($answer); 
-			
-			$html=<<<EOL
-			<td class="header_{$header} {$border} {$setUnderb}" 
-			colspan="{$colspan}">
-														
-			<span class="header_{$header}">
-			{$h_answer}
-			</span>
-			</td>
-
-			EOL;
-			return $html;
-		}
+		
 	?>
 
 	<table id="results" target="a">
