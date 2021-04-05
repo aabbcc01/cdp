a6aebae1a2d8b2ea298ef204059f06a07679b45b
aabbcc01 <pontoppidan1917@yahoo.co.jp>
2021-04-05 13:30:54 +0900

    a

--- DbManager.php
+++ DbManager.php
@@ -1,6 +1,6 @@
 <?php
 function getDb() {
-  $dsn = 'mysql:dbname=cdp; host=127.0.0.1; charset=utf8';
+  $dsn = 'mysql:dbname=cdp;port=3306; host=127.0.0.1; charset=utf8';
   $usr = 'root';
   $passwd = 'Tokissme?';
 
